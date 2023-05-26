f4a67879d8e4dbe67611c09146bf870b17a7033c
KiritoKing <kiritoclzh@gmail.com>
2023-05-27 03:10:44 +0800

    feat(front-end): 测试lint-staged

--- packages/front-end/src/index.tsx
+++ packages/front-end/src/index.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 import "./index.css";
 import "antd/dist/antd.css";
 
-document.title = "人员管理系统";
+document.title = "人员管理系统 - HRMS";
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
